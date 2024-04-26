0945e89a00c6719120012fac6092d42427163d65
Joselop18 <aaguilar-2023496@kinal.edu.gt>
2024-04-26 04:24:17 -0600

    Initial Commit

---
+++ src/org/joseaguilar/dto/ClienteDTO.java
@@ -0,0 +1,26 @@
+package org.joseaguilar.dto;
+
+import org.joseaguilar.model.Cliente;
+
+public class ClienteDTO {
+   private static ClienteDTO instance;
+   private Cliente cliente;
+   
+   private ClienteDTO(){
+       
+   }
+   
+   public static ClienteDTO getClienteDTO(){
+       if(instance == null){
+           instance = new ClienteDTO();
+       }
+       return instance;
+   }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
